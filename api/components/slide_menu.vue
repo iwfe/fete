bdd46f2bb952db77d4f9692f5f3f8c2a80d89d54
wjs <weijinsh@gmail.com>
2016-06-23 18:13:14 +0800

    add slide menu component

---
+++ api/components/slide_menu.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="slide-menu">
+        <slot></slot>
+    </div>
+</template>
+
+<script type="text/babel">
+    export default {
+        data () {
+            return {
+                open: false
+            }
+        },
+        ready () {
+            $('html').click(() => {
+                this.$emit('slide-menu-close');
+            });
+            $('.slide-menu').click((e) => {
+                e.stopPropagation();
+            });
+        },
+        events: {
+            'slide-menu-open': function () {
+                if (!this.open) {
+                    $('.slide-menu').transition('slide left');
+                    this.open = true;
+                }
+            },
+            'slide-menu-close': function () {
+                if (this.open) {
+                    $('.slide-menu').transition('slide left');
+                    this.open = false;
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="sass" rel="stylesheet/scss" type="text/css">
+    .slide-menu {
+        display: none;
+        position: fixed;
+        top: 0;
+        left: 50%;
+        width: 50%;
+        height: 100%;
+        background-color: #fff;
+        -webkit-box-shadow: 0px 0px 3px 2px rgba(0,0,0,.2);
+        -moz-box-shadow: 0px 0px 3px 2px rgba(0,0,0,.2);
+        box-shadow: 0px 0px 3px 2px rgba(0,0,0,.2);
+    }
+</style>
--- api/index.vue
+++ api/index.vue
@@ -1,16 +1,23 @@
 <template>
     <div>
-        <div v-for="item in apiList">
+        <div class="list-item"
+            v-for="item in apiList"
+            @click="showDetail(item.id, $event)">
             {{item.title}} -- {{item.url}} -- {{item.method}}
         </div>
+
+        <slide-menu></slide-menu>
     </div>
 </template>
 
 <script type="text/babel">
     import TopNav from './top_nav.vue';
+    import SlideMenu from './components/slide_menu.vue';
 
     export default {
-        components: {},
+        components: {
+            SlideMenu
+        },
         data () {
             return {
                 apiList: []
@@ -29,11 +36,24 @@
                 }).then(res => {
                     this.apiList = res.data;
                 });
+            },
+            showDetail (id, e) {
+                this.$broadcast('slide-menu-open');
+                e.stopPropagation();   // 阻止冒泡
             }
         }
     }
 </script>
 
 <style lang="sass" rel="stylesheet/scss" type="text/css">
-
+    .list-item {
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #eee;
+        margin-bottom: -1px;
+        cursor: pointer;
+        &:hover {
+            background-color: #fafafa;
+        }
+    }
 </style>

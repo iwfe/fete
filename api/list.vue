727d2b5d6b5b4387840e7666da3b4d99d4bcd219
白文浩 <bwh2009@qq.com>
2016-06-23 16:14:20 +0800

    add

---
+++ api/list.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script type="text/babel">
+    export default {
+        components: {},
+        data () {
+            return {
+                list: []
+            }
+        },
+        props: {},
+        ready() {
+            this.getList();
+        },
+        methods: {
+            getList () {
+                fetch('/api/apis', {
+                    body: {prdId: '111'}
+                }).then(res => {
+                    this.list = res.data;
+                });
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>

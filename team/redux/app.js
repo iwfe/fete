89b07e37a2aa2655704c149775a509713a53ca78
zyy <zyyjade@126.com>
2016-06-27 19:29:24 +0800

    cc

--- team/redux/app.js
+++ team/redux/app.js
@@ -2,106 +2,29 @@
  * @Author: jade
  * @Date:   2016-06-26 21:57:58
  * @Last Modified by:   jade
- * @Last Modified time: 2016-06-26 22:14:39
+ * @Last Modified time: 2016-06-27 18:21:39
  */
 
 'use strict';
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import {
-    Router,
-    Route,
-    IndexRoute,
-    Link,
-    browserHistory
-} from 'react-router';
+import { connect } from 'react-redux';
+import * as actions from '../action'
+// import { selectReddit, fetchPostsIfNeeded, invalidateReddit } from './action';
+// import {
+//     Router,
+//     Route,
+//     IndexRoute,
+//     Link,
+//     browserHistory
+// } from 'react-router';
 import Button from 'antd/lib/button';
 import Icon from 'antd/lib/icon';
 import Card from 'antd/lib/card';
 import Col from 'antd/lib/col';
 import Row from 'antd/lib/row';
 import Team from './team';
-import './index.scss';
-
-const Index = React.createClass({
-    render() {
-        return (
-            <div >
-                <h1>我的团队</h1>
-                {this.props.children}
-            </div>
-        )
-    }
-});
-
-const Home = React.createClass({
-    getInitialState() {
-        return {
-            teams: []
-        }
-    },
-    componentDidMount() {
-        const self = this;
-        fetch('/team/list').then(res => {
-            self.setState({
-                teams: res.data
-            });
-        });
-    },
-
-    handleTeam() {
-        let teams = this.state.teams;
-        teams.push({
-            opt: 'add'
-        });
-        this.setState({
-            teams: teams
-        });
-    },
-
-    render() {
-        const state = this.state;
-        return (
-            <div className="mod-team">
-                <Row gutter={16}>
-                    {
-                        state.teams.map(item=>{
-                            <Col key={'team-' + item._id} className="gutter-row" span={6}>
-                                <Team team={item}></Team>
-                            </Col>
-                        })
-                    }
-                    <Col className="team-add gutter-row" span={6} onClick={this.handleTeam}>
-                        <Card title="创建团队"><Icon type="plus" /></Card>
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
-});
-
-// const Item = React.createClass({});
-
-// const Team = React.createClass({});
-//<Route path=":id" component={Team} />
-//<IndexRoute component={Index}/>
-//<Route path="team/:id" component={Home} />
-ReactDOM.render((
-    <Router history={browserHistory}>
-        <Route path="/team" component={Index}>
-            <IndexRoute component={Home}/>
-            <Route path=":id" component={Home} />
-        </Route>
-    </Router>
-), document.getElementById('main'));
-
-
-
-import React, { Component, PropTypes } from 'react'
-import { connect } from 'react-redux'
-import { selectReddit, fetchPostsIfNeeded, invalidateReddit } from '../actions'
-import Picker from '../components/Picker'
-import Posts from '../components/Posts'
+import './app.scss';
 
 class App extends Component {
     constructor(props) {
@@ -176,30 +99,114 @@ class App extends Component {
 }
 
 App.propTypes = {
-    selectedReddit: PropTypes.string.isRequired,
-    posts: PropTypes.array.isRequired,
-    isFetching: PropTypes.bool.isRequired,
-    lastUpdated: PropTypes.number,
+    // selectedReddit: PropTypes.string.isRequired,
+    teams: PropTypes.array.isRequired,
+    // isFetching: PropTypes.bool.isRequired,
+    // lastUpdated: PropTypes.number,
     dispatch: PropTypes.func.isRequired
 }
 
-function mapStateToProps(state) {
-    const { selectedReddit, postsByReddit } = state
-    const {
-        isFetching,
-        lastUpdated,
-        items: posts
-    } = postsByReddit[selectedReddit] || {
-        isFetching: true,
-        items: []
-    }
+// function mapStateToProps(state) {
+//     // const { selectedReddit, postsByReddit } = state
+//     const {
+//         isFetching,
+//         teams: []
+//     } = postsByReddit[selectedReddit] || {
+//         isFetching: true,
+//         items: []
+//     }
 
-    return {
-        selectedReddit,
-        posts,
-        isFetching,
-        lastUpdated
-    }
-}
+//     return {
+//         selectedReddit,
+//         teams,
+//         isFetching,
+//         lastUpdated
+//     }
+// }
 
-export default connect(mapStateToProps)(App)
+export default connect(()=>{
+    teams:[]
+}, action)(App);
+
+// const App = React.createClass({
+//     render() {
+//         return (
+//             <div >
+//                 <h1>我的团队</h1>
+//                 {this.props.children}
+//             </div>
+//         )
+//     }
+// });
+
+// const Home = React.createClass({
+//     getInitialState() {
+//         return {
+//             teams: []
+//         }
+//     },
+//     componentDidMount() {
+//         const self = this;
+//         fetch('/team/list').then(res => {
+//             self.setState({
+//                 teams: res.data
+//             });
+//         });
+//     },
+
+//     handleTeam() {
+//         let teams = this.state.teams;
+//         teams.push({
+//             opt: 'add'
+//         });
+//         this.setState({
+//             teams: teams
+//         });
+//     },
+
+//     render() {
+//         const state = this.state;
+//         return (
+//             <div className="mod-team">
+//                 <Row gutter={16}>
+//                     {
+//                         state.teams.map(item=>{
+//                             <Col key={'team-' + item._id} className="gutter-row" span={6}>
+//                                 <Team team={item}></Team>
+//                             </Col>
+//                         })
+//                     }
+//                     <Col className="team-add gutter-row" span={6} onClick={this.handleTeam}>
+//                         <Card title="创建团队"><Icon type="plus" /></Card>
+//                     </Col>
+//                 </Row>
+//             </div>
+//         )
+//     }
+// });
+
+// const Item = React.createClass({});
+
+// const Team = React.createClass({});
+//<Route path=":id" component={Team} />
+//<IndexRoute component={Index}/>
+//<Route path="team/:id" component={Home} />
+// ReactDOM.render((
+//     <Router history={browserHistory}>
+//         <Route path="/team" component={Index}>
+//             <IndexRoute component={Home}/>
+//             <Route path=":id" component={Home} />
+//         </Route>
+//     </Router>
+// ), document.getElementById('main'));
+// 
+export default App;
+
+
+
+
+
+
+// import Picker from '../components/Picker'
+// import Posts from '../components/Posts'
+

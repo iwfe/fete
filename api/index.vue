abb06842484dd0b48e6c5c6903dcddbe5edad030
白文浩 <bwh2009@qq.com>
2016-06-28 10:34:03 +0800

    add

--- api/index.vue
+++ api/index.vue
@@ -11,7 +11,6 @@
     import TopNav from './top_nav.vue';
     import SlideMenu from './components/slide_menu.vue';
     import ApiSetting from './api_setting.vue';
-    import store from './vuex/store.js'
     export default {
         store,
         components: {

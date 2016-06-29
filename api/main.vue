2e6039148812102629d821a02d779f4baee5419c
白文浩 <bwh2009@qq.com>
2016-06-29 17:46:40 +0800

    add

--- api/main.vue
+++ api/main.vue
@@ -3,7 +3,6 @@
     <path></path>
     <bar></bar>
     <list></list>
-
     <slide-menu v-if="$route.name === 'list'">
       <api-setting></api-setting>
     </slide-menu>
@@ -11,14 +10,15 @@
 </template>
 
 <script type="text/babel">
+import store from './vuex/store.js'
 import List from './list.vue'
 import Bar from './bar.vue'
 import Path from './path.vue'
-
 import SlideMenu from './components/slide_menu.vue';
 import ApiSetting from './api_setting.vue';
 
 export default {
+  store,
   components: {
     List,
     Path,

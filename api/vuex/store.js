dd3ca9502f7b4f0831b4e05106c46d7d419c1886
GeYuanjun <daydaygyj@gmail.com>
2016-06-30 10:35:53 +0800

    modify judge

--- api/vuex/store.js
+++ api/vuex/store.js
@@ -3,7 +3,7 @@
 * @Date:   2016-06-29 11:33:13
 * @Email:  geyuanjun.sh@superjia.com
 * @Last modified by:   geyuanjun
-* @Last modified time: 2016-06-29 20:21:8
+* @Last modified time: 2016-06-30 10:27:9
 */
 import Vuex from 'vuex'
 Vue.use(Vuex)
@@ -17,7 +17,7 @@ const state = {
 
 const mutations = {
   ADD(State, list) { // 添加
-    if (!list.id) {
+    if (!list.url) {
       list = {
         url: '-',
         title: '-',

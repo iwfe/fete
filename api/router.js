7a825c7e1769c9d6fa55ec1867e9247b9775f136
wjs <weijinsh@gmail.com>
2016-07-11 19:47:13 +0800

    api_check, get mock data with ajax

--- api/router.js
+++ api/router.js
@@ -213,29 +213,36 @@ router.get('/mock_check.js', sutil.setRouterParams, function*(next) {
     this.type = 'js'
     this.body = `console.log('no projectId');`;
   } else {
+
+    let jsContent = `
+                    var feteApiProjectId = '${this.parse.projectId}';
+                    var feteApiUseMockData = ${this.parse.useMockData === 'true' ? true : false};
+                    var feteApiHost = '${config.host}';
+                    var feteApiForMock = {};
+                    `;
+    jsContent += fs.readFileSync(path.resolve('common/api_check.js'), 'utf8')
+    this.type = 'js'
+    this.body = jsContent
+  }
+  return false
+})
+router.get('/api_mock_data', sutil.setRouterParams, sutil.allowCORS, function*(next) {
+  if (!this.parse.projectId) {
+    sutil.failed(this, 1003)
+  } else {
     let apiItems = yield apiDao.find({projectId: this.parse.projectId}, {
       fields: { _id: 0, url: 1, method: 1, input: 1, output: 1, root: 1 },
       sort: { createAt: 1 }
     })
-    let allApiFormMock = {}
+    let allApiForMock = {}
     _.each(apiItems, item => {
-      allApiFormMock[item.method + item.root + item.url] = {
+      allApiForMock[item.method + item.root + item.url] = {
         input: item.input,
         output: item.output
       }
     })
-    let jsContent = `
-                    var feteApiProductId = '${this.parse.projectId}';
-                    var feteApiUseMockData = ${this.parse.useMockData === 'true' ? true : false};
-                    var feteApiHost = '${config.host}';
-                    var feteApiForMock = ${JSON.stringify(allApiFormMock)};
-                    `;
-    jsContent += fs.readFileSync(path.resolve('common/api_check.js'), 'utf8');
-    this.type = 'js'
-    this.body = jsContent;
+    sutil.success(this, allApiForMock)
   }
-  return false;
-});
-
+})
 
 export default router;

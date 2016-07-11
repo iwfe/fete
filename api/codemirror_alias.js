4fce29d562bf4140ff3a33587401472c3b630472
陈江松 <chenjiangsong.sh@superjia.com>
2016-07-11 19:19:55 +0800

    active line

---
+++ api/codemirror_alias.js
@@ -0,0 +1,17 @@
+/**
+* @Author: chenjiangsong
+* @Date:   2016-07-11 18:07:00
+* @Email:  chenjiangsong.sh@superjia.com
+* @Last modified by:   chenjiangsong
+* @Last modified time: 2016-07-11 19:07:03
+*/
+
+const CodeMirror = require('codemirror/lib/codemirror.js');
+require('codemirror/lib/codemirror.css');
+require('codemirror/addon/lint/lint.css');
+require('codemirror/mode/javascript/javascript.js');
+require('codemirror/mode/css/css.js');
+// require('jsonlint/lib/jsonlint.js');
+require('codemirror/addon/lint/lint.js');
+require('codemirror/addon/lint/json-lint.js');
+require('codemirror/addon/selection/active-line.js')
